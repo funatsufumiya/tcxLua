cdff42a15ffae465c9ad1789f7f57342658b8e0d
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-04-22 19:46:23 +0900

    fix: setBindings made public

--- src/tcxLua.h
+++ src/tcxLua.h
@@ -7,9 +7,9 @@
 class tcxLua {
 public:
     std::shared_ptr<sol::state> getLuaState();
+    void setBindings(const std::shared_ptr<sol::state>& lua);
 
 protected:
-    void setBindings(const std::shared_ptr<sol::state>& lua);
     void setTrussCGeneratedBindings(const std::shared_ptr<sol::state>& lua);
 };
 
